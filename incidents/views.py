c635b6042a68b0a89195f343580daaf15ed021b3
tolase <tolasedairo@gmail.com>
2026-02-19 15:11:10 +0000

    feat: add search and filter functionality to incidents list

--- incidents/views.py
+++ incidents/views.py
@@ -13,14 +13,47 @@ class IncidentListView(ListView):
     model = Incident
     template_name = 'incidents/incident_list.html'
     context_object_name = 'incidents'
-    paginate_by = 10
+    paginate_by = 20
 
     def get_queryset(self):
-        queryset = Incident.objects.all()
+        queryset = Incident.objects.select_related('resident', 'created_by')
+        
+        # Search functionality
+        search_query = self.request.GET.get('search', '')
+        if search_query:
+            from django.db.models import Q
+            queryset = queryset.filter(
+                Q(description__icontains=search_query) |
+                Q(resident__first_name__icontains=search_query) |
+                Q(resident__last_name__icontains=search_query)
+            )
+        
+        # Filter by incident type
+        incident_type = self.request.GET.get('incident_type', '')
+        if incident_type:
+            queryset = queryset.filter(incident_type=incident_type)
+        
+        # Filter by status
+        status = self.request.GET.get('status', '')
+        if status == 'resolved':
+            queryset = queryset.filter(is_resolved=True)
+        elif status == 'open':
+            queryset = queryset.filter(is_resolved=False)
+        
+        # Filter by resident
         resident_id = self.request.GET.get('resident')
         if resident_id:
             queryset = queryset.filter(resident_id=resident_id)
+        
         return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['search_query'] = self.request.GET.get('search', '')
+        context['selected_type'] = self.request.GET.get('incident_type', '')
+        context['selected_status'] = self.request.GET.get('status', '')
+        context['incident_types'] = Incident.INCIDENT_TYPE_CHOICES
+        return context
 
 @method_decorator([login_required, approval_required], name='dispatch')
 class IncidentCreateView(CreateView):
