2e370f9157e10f0efb1f164eddde71d7775ef7dc
tolase <tolasedairo@gmail.com>
2026-02-19 15:08:53 +0000

    feat: register incident model in admin and fix form template rendering

--- incidents/admin.py
+++ incidents/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Incident
 
 # Register your models here.
+@admin.register(Incident)
+class IncidentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'resident', 'incident_type', 'created_by', 'created_at', 'is_resolved']
+    list_filter = ['incident_type', 'is_resolved', 'created_at']
+    search_fields = ['resident__first_name', 'resident__last_name', 'description']
+    readonly_fields = ['created_by', 'created_at']
+    date_hierarchy = 'created_at'
