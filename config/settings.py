bff1f83d4b09d6b69f9ce4ade94f36ba2b423715
tolase <tolasedairo@gmail.com>
2026-02-23 14:31:14 +0000

    remove unwanted space on cloudinary sercure

--- config/settings.py
+++ config/settings.py
@@ -149,7 +149,7 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
-    'SECURE' : True,
+    'SECURE': True,
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
